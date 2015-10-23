618d47e0a83a365b86efe117ed11cd0b49818ecc
holoed@users.noreply.github.com <edmondo@gmail.com>
2015-10-23 16:04:00 +0100

    Add more substitutions tests

--- src/test/scala/SubstitutionsSpec.scala
+++ src/test/scala/SubstitutionsSpec.scala
@@ -7,4 +7,10 @@ class SubstitutionsSpec extends FlatSpec with Matchers {
     (Substitutions.extend("y")(Type.TyVar("T1"))(Subst(Map[String,Type]()))
      should be (Subst(Map("y" -> Type.TyVar("T1")))))
   }
+
+  it should "be extended when not empty" in {
+    (Substitutions.extend("x")(Type.TyVar("T"))(Subst(Map("y" -> Type.TyVar("V"))))
+     should be (Subst(Map("y" -> Type.TyVar("V"),
+                          "x" -> Type.TyVar("T")))))
+  }
 }
